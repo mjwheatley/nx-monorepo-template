b5d022caa00540e6177cf1f73382fe7ebdc65d20
Matthew Wheatley <mattjwheatley@gmail.com>
2026-02-26 09:21:32 -0600

    chore: update lint-staged config to use nx affected commands and improve file handling

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,14 +1,32 @@
 /**
+ * @type {(filenames: string[], delimiter?: string) => string}
+ */
+function filesList(filenames, delimiter = ',') {
+  return filenames
+    .map((filename) => {
+      return filename.replaceAll(/([()[\]])/g, String.raw`\$1`);
+    })
+    .join(delimiter);
+}
+
+/**
  * @type {Record<string, string | string[] | ((filenames: string[]) => string | string[] | Promise<string | string[]>)>}
  */
 export default {
-  'packages/**/*.{ts,js,cjs,mjs,json,md,html,css,scss}': ['vitest related --run --silent=true'],
-  '*.{js,cjs,mjs,ts,cts,mts}': ['eslint --cache --fix'],
-  '*.{js,cjs,mjs,ts,cts,mts,json,yml,yaml,md}': ['cspell lint --no-progress --no-summary --no-must-find-files'],
-  '*': (files) => [
+  '{apps,libs,packages}/**/*.{ts,js,cjs,mjs,json,html,css,scss}': (filenames) => [
+    `nx affected --target=test --files=${filesList(filenames)} --watch=false --coverage=false`,
+  ],
+  '*.{js,cjs,mjs,ts,tsx,cts,mts}': (filenames) => [
+    `nx affected --target=lint --files=${filesList(filenames)} --fix`,
+    `nx affected --target=typecheck --files=${filesList(filenames)}`,
+  ],
+  '*.{js,cjs,mjs,ts,tsx,cts,mts,json,yml,yaml,md}': (filenames) => [
+    `cspell lint --no-progress --no-summary --no-must-find-files ${filesList(filenames, ' ')}`,
+  ],
+  '*': (filenames) => [
     // Spell check file names
-    `sh -c 'echo "${files.join('\n')}" | cspell --show-context stdin'`,
-    `prettier --write --ignore-unknown ${files.join(' ')}`,
-    `secretlint ${files.join(' ')}`,
+    `sh -c 'echo "${filenames.join('\n')}" | cspell --show-context stdin'`,
+    `prettier --write --ignore-unknown ${filesList(filenames, ' ')}`,
+    `secretlint ${filesList(filenames, ' ')}`,
   ],
 };
